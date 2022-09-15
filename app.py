82161f1f9ed566935bfe36ad79bbea0486262104
Qingyang Zhang <az@DN0a1ead1f.SUNet>
2022-09-15 08:19:20 -0700

    test

--- app.py
+++ app.py
@@ -8,12 +8,16 @@ app.config['SECRET_KEY'] = 'fjkesjrelhg'
 @app.route('/bot', methods=['POST'])
 def huhu():
     incoming_msg = request.values['Body']
+    '''
     chat_log = session.get('chat_log')
     answer = ask(incoming_msg, chat_log)
-    session['chat_log'] = append_interaction_to_chat_log(
-        incoming_msg, answer, chat_log)
+    session['chat_log'] = append_interaction_to_chat_log(incoming_msg, 
+                                                         answer, chat_log)
     msg = MessagingResponse()
     msg.message(answer)
+    '''
+    msg = MessagingResponse()
+    msg.message(incoming_msg)
     return str(msg)
 
 if __name__ == '__main__':
