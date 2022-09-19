fbe238cb825650d99d48c030507f97bb64ed9234
Qingyang Zhang <az@DN0a1ead1f.SUNet>
2022-09-18 17:41:02 -0700

    validate bot

--- app.py
+++ app.py
@@ -5,27 +5,25 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'fjkesjrelhg'
 
-@app.route('/bot', methods=['POST'])
+@app.route('/test', methods=['POST'])
+def hey():
+    resp = MessagingResponse()
+    resp.message("The Robots are coming! Head for the hills!")
+    return str(resp)
+
+
+
+@app.route('/bot', methods=['GET','POST'])
 def huhu():
     incoming_msg = request.values['Body']
-    '''
+    
     chat_log = session.get('chat_log')
     answer = ask(incoming_msg, chat_log)
     session['chat_log'] = append_interaction_to_chat_log(incoming_msg, 
                                                          answer, chat_log)
     msg = MessagingResponse()
     msg.message(answer)
-    msg = MessagingResponse()
-    msg.message(incoming_msg)
     return str(msg)
-    '''
-    resp = MessagingResponse()
-
-    # Add a message
-    resp.message("The Robots are coming! Head for the hills!")
-
-    return str(resp)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
