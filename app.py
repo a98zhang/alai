9d576fcbe73e3c834c763f920b51dcafd7505d74
Qingyang Zhang <az@DN0a1e4f9f.SUNet>
2022-09-25 19:47:37 -0700

    implement changing prompt

--- app.py
+++ app.py
@@ -1,39 +1,45 @@
 from flask import Flask, request, session
 from twilio.twiml.messaging_response import MessagingResponse
-from bot import ask, change_robot, validate_chat_log, update_chat_log
+import bot 
+#from bot import ask, change_robot, validate_chat_log, update_chat_log
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'fjkesjrelhg'
+bot = bot.Bot()
 
 @app.route('/bot', methods=['GET','POST'])
-def bot():
+def gpt():
+
     # fetch the incoming message
     incoming_msg = request.values['Body']
 
     # special cases
     if incoming_msg == 'RESTART':           # reset the conversation
         session['chat_log'] = None
-        return str(MessagingResponse().message('Hi! Ask me anything.'))
+        return bot.answer('Hi! Ask me anything.')
+
     elif incoming_msg == 'CHANGE':          # prompt for changing robot
-        return str(MessagingResponse().message('Who do you want to talk to? A. Prim; B. Jabe. Answer A or B'))
-    elif change_robot(incoming_msg):          # robot changed
+        bot.prompt_to_be_changed = True
+        return bot.answer('Who do you want to talk to? "Primer", "Jabe", or "Nietzsche"')
+
+    elif bot.prompt_to_be_changed:          # robot changed
+        bot.change_prompt(incoming_msg)
         session['chat_log'] = None
+        return bot.answer(f'Hi! You are talking to {incoming_msg} now. Ask me anything.')
 
     # fetch the chat log and fix if necessary
-    chat_log = validate_chat_log(session.get('chat_log'), 
+    chat_log = bot.validate_chat_log(session.get('chat_log'), 
                                  incoming_msg)
     
     # retrieve the answer to the incoming message from bot
-    answer = ask(incoming_msg, chat_log)
+    answer = bot.ask(incoming_msg, chat_log)
     
     # update chat log by appending the current interactions
-    session['chat_log'] = update_chat_log(incoming_msg, 
+    session['chat_log'] = bot.update_chat_log(incoming_msg, 
                                           answer, chat_log)
     
     # send back the message 
-    msg = MessagingResponse()
-    msg.message(answer)
-    return str(msg)
+    return bot.answer(answer)
 
 # TODO: Error handling
 @app.errorhandler(400)
--- bot.py
+++ bot.py
@@ -9,48 +9,61 @@ load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 completion = openai.Completion()
 
-presets = pd.read_csv('data/presets.csv').set_index('preset')
-start_sequence = presets.loc['Default']['start_sequence']
-restart_sequence = presets.loc['Default']['restart_sequence']
-session_prompt = presets.loc['Default']['session_prompt']
-n_tokens = 1000     # shared between prompt and completion
+class Bot(object):
+    def __init__(self, preset='Default'):
+        self.presets = pd.read_csv('data/presets.csv').set_index('preset')
+        self.start_sequence = self.presets.loc[preset]['start_sequence']
+        self.restart_sequence = self.presets.loc[preset]['restart_sequence']
+        self.session_prompt = self.presets.loc[preset]['session_prompt']
+        self.n_tokens = 1000     # shared between prompt and completion
+        self.prompt_to_be_changed = False
 
-def ask(question, chat_log=None):
-    if chat_log is None:
-        chat_log_texts = ''
-    else:
-        chat_log_texts = ''.join(chat_log)
-    prompt_text = f'{session_prompt}{chat_log_texts}{restart_sequence} {question}{start_sequence}'
+    def ask(self, question, chat_log=None):
+        if chat_log is None:
+            chat_log_texts = ''
+        else:
+            chat_log_texts = ''.join(chat_log)
+        prompt_text = f'{self.session_prompt}{chat_log_texts}{self.restart_sequence} {question}{self.start_sequence}'
 
-    response = openai.Completion.create(
-        model="text-davinci-002",
-        prompt=prompt_text,
-        temperature=0.8,
-        max_tokens=n_tokens,
-        top_p=1,
-        frequency_penalty=0.5, 
-        presence_penalty=0.2,
-        stop=["\n"]
-    )
-    story = response['choices'][0]['text']
+        response = openai.Completion.create(
+            model="text-davinci-002",
+            prompt=prompt_text,
+            temperature=0.8,
+            max_tokens=self.n_tokens,
+            top_p=1,
+            frequency_penalty=0.5, 
+            presence_penalty=0.2,
+            stop=["\n"]
+        )
+        story = response['choices'][0]['text']
+        
+        # report 'no answer' response detail
+        if str(story) == '':
+            print('the response is ', response)
+        return str(story)
+
+    def change_prompt(self, incoming_msg):
+        self.start_sequence = self.presets.loc[incoming_msg]['start_sequence']
+        self.restart_sequence = self.presets.loc[incoming_msg]['restart_sequence']
+        self.session_prompt = self.presets.loc[incoming_msg]['session_prompt']
+        self.prompt_to_be_changed = False
+        print('----PROMPT HAS BEEN CHANGED----')
+        return None
+
+    def validate_chat_log(self, chat_log, incoming_msg=None):
+        if chat_log is None:
+            return []
+
+        # 1 token ~= 4 chars in English
+        if len(''.join(chat_log)) >= ((self.n_tokens-300) * 4):
+            chat_log.pop(0)
+        return chat_log
+
+    def update_chat_log(self, question, answer, chat_log=None):
+        chat_log.append(f'{self.restart_sequence} {question}{self.start_sequence}{answer}') 
+        return chat_log
     
-    # report 'no answer' response detail
-    if str(story) == '':
-        print('the response is ', response)
-    return str(story)
-
-#def change_robot(incoming_msg):
-
-
-def validate_chat_log(chat_log, incoming_msg=None):
-    if chat_log is None:
-        return []
-
-    # 1 token ~= 4 chars in English
-    if len(''.join(chat_log)) >= ((n_tokens-300) * 4):
-        chat_log.pop(0)
-    return chat_log
-
-def update_chat_log(question, answer, chat_log=None):
-    chat_log.append(f'{restart_sequence} {question}{start_sequence}{answer}') 
-    return chat_log
+    def answer(self, answer):
+        msg = MessagingResponse()
+        msg.message(answer)
+        return str(msg)
