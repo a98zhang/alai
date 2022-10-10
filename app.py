5ae36d2549725e2ea01966c33b49a16204ae20a4
qingyang <zhang.qingyang@outlook.com>
2022-10-10 00:55:53 -0700

    session preset info

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, session
-from twilio.twiml.messaging_response import MessagingResponse
 import bot 
 
 app = Flask(__name__)
@@ -10,44 +9,49 @@ bot = bot.Bot()
 @app.route('/bot', methods=['GET','POST'])
 def gpt():
 
-    # fetch the incoming message
+    if not session.get('preset'):               # default preset 
+        session['preset'] = 'Default'
+
+    curr_bot = session['preset']
     incoming_msg = request.values['Body']
+    chat_log = bot.validate_chat_log(session.get('chat_log'))
 
     # special cases
     if incoming_msg == 'RESTART':               # reset the conversation
-        session['chat_log'] = None
+        session.pop('chat_log', None)
+        session.pop('preset', None)
         return bot.answer('Hi! Ask me anything.')
 
     elif incoming_msg == 'CHANGE':              # prompt for changing robot
-        bot.prompt_to_be_changed = True
+        session['prompt_to_be_changed'] = True
         return bot.answer('Who shall I call for you?')
 
-    elif incoming_msg == 'LIST':
+    elif incoming_msg == 'LIST':                # list all presets
         preset_list = bot.list_all_presets()
         return bot.answer(f'Here is the complete list:{preset_list}')
 
-    elif bot.prompt_to_be_changed:          
-        if bot.is_in_presets(incoming_msg):   # change prompt
-            preset_name = bot.change_prompt(incoming_msg)
-            session['chat_log'] = None
-            return bot.answer(f'Hi. {preset_name} here. What shall we discuss?')
-        else:                                   # prompt not in presets
-            return bot.answer(f'{incoming_msg} is not answering. Try another one.')
-
+    elif session.get('prompt_to_be_changed'):   # change the current preset
+        # change bot       
+        if bot.is_in_presets(incoming_msg):   
+            session.pop('chat_log', None)
+            session.pop('prompt_to_be_changed', None)
+            session['preset'] = bot.validate_preset_name(incoming_msg)
+            print(f'-------{session["preset"]}------' )
+            return bot.answer(
+                f'Hi. {session["preset"]} here. What shall we discuss?'
+            )
+        # prompt not in presets                          
+        return bot.answer(f'{incoming_msg} is not answering. Try another one.')
+        
     elif incoming_msg == 'DEBUG':
         bot.debug(session.get('chat_log'))
     
-    # fetch the chat log and fix if necessary
-    chat_log = bot.validate_chat_log(session.get('chat_log'))
-    
     # retrieve the answer to the incoming message from bot
-    answer = bot.ask(incoming_msg, chat_log)
+    answer = bot.ask(incoming_msg, curr_bot, chat_log)
 
     # update chat log by appending the current interactions
     session['chat_log'] = bot.update_chat_log(incoming_msg, 
-                                          answer, chat_log)
-    
-    print(answer)
+                                answer, curr_bot, chat_log)
 
     # send back the message 
     return bot.answer(answer)
